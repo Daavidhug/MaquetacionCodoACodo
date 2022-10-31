629fc189597b7699495534894e7e161e6df4b161
David Hug <hugirvp@hotmail.com>
2022-10-31 01:23:32 -0300

    js ya esta haciendo bien el calculo del total

--- javaScript.js
+++ javaScript.js
@@ -2,9 +2,10 @@ const PRECIO = 200 ;
 
 function resumen(){
     let cantidadDeEntrada = document.getElementById('cantidadEntrada').value
-    let categoriaSeleccionada = document.getElementById('categoriaDeDescuento').value
-    let precioTotal = PRECIO * cantidadDeEntrada;
+    let descuentoPorCategoria = document.getElementById('categoriaDeDescuento').value
+    let subTotal = (PRECIO * cantidadDeEntrada);
+    let descuento = (descuentoPorCategoria / 100) * subTotal ;
+    let precioTotal = subTotal - descuento;
 
     console.log(precioTotal);
-    console.log(categoriaSeleccionada);
 }
