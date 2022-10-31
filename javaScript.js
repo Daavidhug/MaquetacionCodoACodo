9b5623dd587edb276c6b440b42c8316caf3d651f
David Hug <hugirvp@hotmail.com>
2022-10-31 00:53:51 -0300

    cambie el total de un input a un p y la funcion de js esta andando

--- javaScript.js
+++ javaScript.js
@@ -1,11 +1,10 @@
-
-var precio = 200 ;
-
-let resultado = precio;
-
+const PRECIO = 200 ;
 
 function resumen(){
-    console.log(precio);
+    let cantidadDeEntrada = document.getElementById('cantidadEntrada').value
+    let categoriaSeleccionada = document.getElementById('categoriaDeDescuento').value
+    let precioTotal = PRECIO * cantidadDeEntrada;
 
+    console.log(precioTotal);
+    console.log(categoriaSeleccionada);
 }
-
